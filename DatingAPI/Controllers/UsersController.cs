83fb8e09aecfedd9702c003a5d0aafff25202bd8
Jefferson <jeffer_16_99@hotmail.com>
2021-07-31 18:36:10 -0500

    Agregar archivos de proyecto.

---
+++ DatingAPI/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using DatingAPI.Data;
+using DatingAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context) {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<AppUser>> GetUsers() {
+            return await this._context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> FindUserById(int id) {
+            var user = await this._context.Users.FindAsync(id);
+            if (user is null) {
+                return BadRequest("User Not Found");
+            }
+            return Ok(user);
+        }
+
+    }
+}
